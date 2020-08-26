0c8b5e93ce7ce96714859cdcab0759d692bcc3b7
chin <cteeeri@gmail.com>
2020-08-26 17:15:19 -0500

    init

---
+++ dira/myfileone.py
@@ -0,0 +1,7 @@
+def hello():
+    return f'dira fileone hello'
+
+
+if __name__ == "__main__":
+    a = hello()
+    print(a)
---
+++ dirb/filebb.py
@@ -0,0 +1,10 @@
+from dira.myfileone import hello
+
+
+def bbhello():
+    return f'{hello()} > added dirb with Chin'
+
+
+if __name__ == "__main__":
+    print(bbhello())
+
